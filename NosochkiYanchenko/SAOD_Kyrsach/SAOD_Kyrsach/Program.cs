9484ec3e733bd22641723cc3b3f23f39ba265c8d
Карпова Яна Алексеевна <ip316s08@csc.sibsutis.ru>
2024-07-22 19:23:50 +0700

    массив объектов

--- NosochkiYanchenko/SAOD_Kyrsach/SAOD_Kyrsach/Program.cs
+++ NosochkiYanchenko/SAOD_Kyrsach/SAOD_Kyrsach/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.Text;
+﻿using SAOD_Kyrsach;
+using System.Text;
 
 class MainClass
 {
     static void Main()
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        List<Book> ListBook = new List<Book>();
         using (FileStream fs = new FileStream(@"./testBase1.dat",
                    FileMode.Open))
         {
@@ -20,20 +22,27 @@ class MainClass
 
             using (BinaryReader br = new BinaryReader(fs))
             {
-                int count = fs.Read(author);
-                count = fs.Read(title);
-                count = fs.Read(publisher);
-                yearPublish = br.ReadInt16();
-                countPages = br.ReadInt16();
+                while (fs.Read(author) != 0)
+                {
+                    //int count = fs.Read(author);
+                    int count = fs.Read(title);
+                    count = fs.Read(publisher);
+                    yearPublish = br.ReadInt16();
+                    countPages = br.ReadInt16();
 
-                string authorString =
-                    Encoding.Unicode.GetString(Encoding.Convert(Encoding.GetEncoding(866), Encoding.Unicode, author));
-                string titleString =
-                    Encoding.Unicode.GetString(Encoding.Convert(Encoding.GetEncoding(866), Encoding.Unicode, title));
-                string publisherString =
-                    Encoding.Unicode.GetString(Encoding.Convert(Encoding.GetEncoding(866), Encoding.Unicode, publisher));
-                Console.WriteLine($"{authorString} {titleString} {publisherString} {yearPublish} {countPages}");
+                    string authorString =
+                        Encoding.Unicode.GetString(Encoding.Convert(Encoding.GetEncoding(866), Encoding.Unicode, author));
+                    string titleString =
+                        Encoding.Unicode.GetString(Encoding.Convert(Encoding.GetEncoding(866), Encoding.Unicode, title));
+                    string publisherString =
+                        Encoding.Unicode.GetString(Encoding.Convert(Encoding.GetEncoding(866), Encoding.Unicode, publisher));
+                    ListBook.Add(new Book(authorString, titleString, publisherString, yearPublish, countPages));
+                }
             }
+        }
+        foreach(Book item in ListBook)
+        {
+            Console.WriteLine(item);
         }
     }
 }
