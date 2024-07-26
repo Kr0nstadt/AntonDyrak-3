47d9aaeda593d3f76e058c39b11c8160cb5fdaff
QQQ <QQQ>
2024-07-26 21:22:07 +0700

    Digital sort

---
+++ NosochkiYanchenko/SAOD_Kyrsach/SAOD_Kyrsach/DigitalSort/DigitalSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD_Kyrsach.DigitalSort
+{
+    internal static class DigitalSort
+    {
+        public static void Sort(IList<IByteGetter> list)
+        {
+            Queue<IByteGetter> queue = new Queue<IByteGetter>(list);
+            const int nQueues = 256;
+            Queue<IByteGetter>[] tempQueues = new Queue<IByteGetter>[nQueues];//тут точно 256?
+            tempQueues = tempQueues.Select(q => new Queue<IByteGetter>()).ToArray();
+            int maxBytesCount = queue.Max(x => x.CountByte);
+            for (int i = 0; i < maxBytesCount; ++i)
+            {
+                while (queue.Count > 0)
+                {
+                    IByteGetter qItem = queue.Dequeue();
+                    tempQueues[qItem.GetByte(i)].Enqueue(qItem);
+                }
+
+                foreach (Queue<IByteGetter> q in tempQueues)
+                {
+                    while (q.Count > 0)
+                    {
+                        queue.Enqueue(q.Dequeue());
+                    }
+                }
+            }
+
+            list.Clear();
+            
+            foreach(IByteGetter byteGetter in queue)
+            {
+                list.Add(byteGetter);
+                //Console.WriteLine(byteGetter.ToString());
+            }
+        }
+    }
+}
